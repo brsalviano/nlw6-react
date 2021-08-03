1738e0dd79bdcd06a1367f08a6c646bbf5d7453b
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-03 17:54:24 -0300

    Estado

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,18 @@
-type ButtonProps = {
-    children?: string;
-}
+import { useState } from "react";
 
-export function Button(props: ButtonProps) {
+export function Button() {
+
+    //criando um estado...
+    const [counter, setCounter] = useState(0)
+
+    function increment() {
+      //manipulando o valor do estado.
+      //Imutavel! Nós não estamos mudando o valor de counter, mas sim atribuindo um novo counter
+      setCounter(counter + 1);
+    }
+
     return (
-        <button>{props.children || 'Default'}</button>
+        <button onClick={increment}>{counter}</button>
     )
 }
 
