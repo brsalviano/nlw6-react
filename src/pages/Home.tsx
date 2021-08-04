99dae8cac8ece456aeecf955f6525883cdec13e3
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-04 17:30:42 -0300

    Refatoração e criação do hook de autenticação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
-import { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
 
-import { AuthContext } from '../App';
+import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -10,11 +8,12 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
+import { useAuth } from '../hooks/useAuth';
 
 
 export function Home() {
     const history = useHistory();
-    const { user, signInWithGoogle } = useContext(AuthContext)
+    const { user, signInWithGoogle } = useAuth();
 
     async function handleCreateRoom() {
 
